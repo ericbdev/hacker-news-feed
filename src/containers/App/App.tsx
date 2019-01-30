0e65ea4ea6a1660ba09e287b48b792360e9b7f6f
Eric <ericbdev@gmail.com>
2019-01-29 23:56:24 -0500

    Story styling
    - Style Stories, and abstract story to own component

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,9 +1,35 @@
 import * as React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import Story from '../../types/Story';
+import StoryCard from '../../components/StoryCard';
+
+const styles = theme => ({
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  header: {
+    marginTop: theme.spacing.unit * 2,
+  },
+  mainGrid: {
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
+  },
+});
 
 type P = {
   fetchTopStories(): void;
   stories: Array<Story>;
+  classes: any;
 };
 
 type S = {
@@ -20,17 +46,35 @@ class App extends React.Component<P, S> {
   };
 
   render() {
-    const { stories } = this.props;
-    
+    const { stories, classes } = this.props;
+
     return (
-      <div>
-        <h1>Hacker News Feed</h1>
-        {stories.map(story => (
-          <div key={story.id}>{story.title}</div>
-        ))}
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <div className={classes.layout}>
+          <header className={classes.header}>
+            <Typography
+              component='h1'
+              variant='h5'
+              color='inherit'
+              align='center'
+              noWrap
+            >
+              Hacker News Feed
+            </Typography>
+          </header>
+
+          <main>
+            <Grid container spacing={16} className={classes.mainGrid}>
+              {stories.map(story => (
+                <StoryCard story={story} key={story.id}/>
+              ))}
+            </Grid>
+          </main>
+        </div>
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
